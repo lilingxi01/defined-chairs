05b697c911a56f84045e75741626225de919ee82
Shevchik <Shev4ik.den@gmail.com>
2013-09-01 17:21:24 +0400

    Remove unused import.

--- src/com/cnaude/chairs/Chairs.java
+++ src/com/cnaude/chairs/Chairs.java
@@ -2,7 +2,6 @@ package com.cnaude.chairs;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
