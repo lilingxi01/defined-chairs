c13318bcab24efb9c19e3d53334365f3f5d3beea
Shevchik <Shev4ik.den@gmail.com>
2013-12-14 21:50:42 +0400

    Remove unused import

--- src/com/cnaude/chairs/Chairs.java
+++ src/com/cnaude/chairs/Chairs.java
@@ -22,7 +22,6 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
