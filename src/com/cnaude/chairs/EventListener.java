424f6e5daa0a3e869783eda7d4e29b60b7ae93e8
Shevchik <Shev4ik.den@gmail.com>
2013-12-14 14:59:59 +0400

    Remove unused import

--- src/com/cnaude/chairs/EventListener.java
+++ src/com/cnaude/chairs/EventListener.java
@@ -14,7 +14,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
 import org.bukkit.material.Stairs;
