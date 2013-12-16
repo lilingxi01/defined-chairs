364d9dbaca07417f29608dc54cde76ad45a938b2
Shevchik <Shev4ik.den@gmail.com>
2013-12-16 23:02:59 +0400

    Null player sit data on disable

--- src/com/cnaude/chairs/Chairs.java
+++ src/com/cnaude/chairs/Chairs.java
@@ -93,6 +93,7 @@ public class Chairs extends JavaPlugin {
         }
         log = null;
         vehiclearrowclass = null;
+        psitdata = null;
     }
     
     public void restartEffectsTask() {
