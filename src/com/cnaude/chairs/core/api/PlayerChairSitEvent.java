364c687e16d21f849141fe163b6b5b031e5fcb27
Shevchik <Shev4ik.den@gmail.com>
2014-03-07 09:17:39 +0400

    Add events classes

---
+++ src/com/cnaude/chairs/core/api/PlayerChairSitEvent.java
@@ -0,0 +1,36 @@
+package com.cnaude.chairs.core.api;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+public class PlayerChairSitEvent extends PlayerEvent implements Cancellable {
+
+	private static final HandlerList handlers = new HandlerList();
+	private boolean cancelled = false;
+	
+	public PlayerChairSitEvent(Player who) {
+		super(who);
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+
+}
