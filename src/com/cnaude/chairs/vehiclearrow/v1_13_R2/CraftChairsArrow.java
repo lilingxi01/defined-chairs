46d9cbd03377705a0bff60c5bcd17c325e6a54ef
I9hdkill <jonathanfriedrich@live.de>
2018-10-08 19:12:18 +0200

    Add 1.13 + 1.13.1

---
+++ src/com/cnaude/chairs/vehiclearrow/v1_13_R2/CraftChairsArrow.java
@@ -0,0 +1,27 @@
+package com.cnaude.chairs.vehiclearrow.v1_13_R2;
+
+import com.cnaude.chairs.api.ChairsAPI;
+import net.minecraft.server.v1_13_R2.EntityArrow;
+import org.bukkit.craftbukkit.v1_13_R2.CraftServer;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftArrow;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Vehicle;
+
+public class CraftChairsArrow extends CraftArrow implements Vehicle {
+
+    public CraftChairsArrow(CraftServer server, EntityArrow entity) {
+        super(server, entity);
+    }
+
+    @Override
+    public void remove() {
+        Entity passenger = getPassenger();
+        if (passenger != null && passenger instanceof Player) {
+            if (ChairsAPI.isSitting((Player) passenger)) {
+                return;
+            }
+        }
+        super.remove();
+    }
+}
